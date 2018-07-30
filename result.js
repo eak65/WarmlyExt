141864e0f0b6589f7c4de1a1ff2e9511d884bcec
Chris Wolf <cw10025@gmail.com>
2018-07-29 23:06:09 -0400

    Tweaking result popup css

--- result.js
+++ result.js
@@ -1,20 +1,12 @@
-
-function get_data() {
-  console.log('get_data() *******....');
-  chrome.storage.local.get('result', function(result) {
-    console.log('Value currently is ' + result.key);
-  });
-}
-
 function render_row(row_data) {
   var row = $("<tr/>");
   $("#warmly_results").append(row);
-  row.append($("<td>" + row_data.summary + "</td>"));
+  row.append($("<td>" + row_data['published date'].substring(0,16) + " keywords: \"" 
+    + row_data.keywords.join(",") + "\"<br/>" + row_data.summary + "</td>"));
 }
 
 document.addEventListener('DOMContentLoaded', function() {
    console.log('result.js *******....');
-   //get_data();
    chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
       console.log('result.js message received...*******....');
       $.getJSON("guido_rossum.json", function(data) {
