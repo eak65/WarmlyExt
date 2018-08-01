408fe0653bf3cb70935f30168768a67bb9519358
Chris Wolf <cw10025@gmail.com>
2018-08-01 17:24:58 -0400

    Add Key to manifest to lock unique extension id

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ var warmly_base_url='https://warmly2.azurewebsites.net/api/1.0/articles';
 
 var result_display = `
   <button class="warmly_btn">Warmly</button>
-  <dialog id='warmly_result'>
-    <h1>Booya</h1>
-    <ul id='results'/>
-  </dialog>
 `;
 
 function refresh(f) {
@@ -19,7 +15,6 @@ function refresh(f) {
 }
 
 function call_warmly(search_term) {
-  result = "";
   $.support.cors = true;
   $.ajax({
       'url' : warmly_base_url,
@@ -29,20 +24,18 @@ function call_warmly(search_term) {
           'term': search_term
       },
       'success' : function(data) {
-          console.log('****>> ' + data['results']); 
-          result = data;
-          $.each(data.results, function(index, value) {
-            $("#results").append('<li>' + value.summary + '</li>');
-          });         
+          console.log('====>> results: ' + data['results']); 
+          var payload = {type: 'warmly_display_result', data: data};
+          console.log('====>> payload type: ' + payload.type + ' payload data: ' + payload.data);
+          chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', payload);
       },
       'error' : function(jqXHR, status, message) {
-          console.log('xxxx>> ' + status + ': ' + message); 
+          console.log('====> service call error: ' + status + ': ' + message); 
           result = '{"results":[{}]}';
+          chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', 
+             {type: 'warmly_display_result', data: data});
       }         
   });
-  var d = document.getElementById('warmly_result');
-  d.show();
-  return result;
 }
 
 var main = function() {
@@ -53,9 +46,15 @@ var main = function() {
     gmail.tools.add_modal_window('Warmly search terms:', '<input id="warmly_criteria" type="text"/>',
       function() {
         var warmly_input = document.getElementById("warmly_criteria");
-        console.log('***** ' + warmly_input);
-        console.log('***** ' + warmly_input.value);
-        console.log('***** ' + call_warmly(warmly_input.value));
+        console.log('====> Search term: ' + warmly_input.value);
+        //console.log('====> ' + call_warmly(warmly_input.value));
+        chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', {type:'warmly_create_popup'});
+        console.log('====> sent message to request popup');
+        /*setTimeout(function() {
+            chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', {type:'warmly_display_result'});
+        }, 1000);*/
+        call_warmly(warmly_input.value);
+        console.log('====> called warmly.');
         gmail.tools.remove_modal_window();
       });
   }, 'L3');
