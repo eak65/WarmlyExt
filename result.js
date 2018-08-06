7661272001507f68f81e0d713f496c64d460bfc9
Chris Wolf <cw10025@gmail.com>
2018-08-06 08:24:35 -0400

    case issue for quotes & snippet

--- result.js
+++ result.js
@@ -5,11 +5,11 @@ function render_row(row_data) {
 
   var td_text = '<p><span class="r_hdr">Summary: </span>' + ss(row_data.summary)
     + '<p><span class="r_hdr">Keywords: </span>' + ss(row_data.keywords).join(",") + "</p>";
-    if (row_data.hasOwnProperty('Quotes')) {
-        td_text += ('<p><span class="r_hdr">Quotes: </span>' + ss(row_data.Quotes) + "</p>");
+    if (row_data.hasOwnProperty('quotes')) {
+        td_text += ('<p><span class="r_hdr">Quotes: </span>' + ss(row_data.quotes) + "</p>");
     }
-    if (row_data.hasOwnProperty('Snippet')) {
-        td_text += ('<p><span class="r_hdr">Snippet: </span>' + ss(row_data.Snippet) + "</p>");
+    if (row_data.hasOwnProperty('snippet')) {
+        td_text += ('<p><span class="r_hdr">Snippet: </span>' + ss(row_data.snippet) + "</p>");
     }
     td_text += ('<p><span class="r_hdr">Source: </span>' + ss(row_data.url) + "</p>"
             + '<span class="r_hdr">Published date: </span>' + ss(row_data['published date']));
