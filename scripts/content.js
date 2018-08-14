a8cafc2bcb7a4ff5f49be1c502b132b8805021c1
brianlee389 <brianlee3369@hotmail.com>
2018-08-13 22:06:21 -0400

    Created a scripts, styles, images folders and put those respective files into those folders

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,16 @@
 
 var j = document.createElement('script');
-j.src = chrome.extension.getURL('jquery-1.10.2.min.js');
+j.src = chrome.extension.getURL('scripts/jquery-1.10.2.min.js');
 (document.head || document.documentElement).appendChild(j);
 
 var g = document.createElement('script');
-g.src = chrome.extension.getURL('gmail.js');
+g.src = chrome.extension.getURL('scripts/gmail.js');
 (document.head || document.documentElement).appendChild(g);
 
 var sel  = document.createElement('script');
-sel.src = chrome.extension.getURL('selectize.js');
+sel.src = chrome.extension.getURL('scripts/selectize.js');
 (document.head || document.documentElement).appendChild(sel);
 
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('main.js');
+s.src = chrome.extension.getURL('scripts/main.js');
 (document.head || document.documentElement).appendChild(s);
