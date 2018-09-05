a74132a037859360ada97f0b428d7ee49571c1c8
Chris Wolf <cw10025@gmail.com>
2018-09-04 22:01:55 -0400

    Add warmly v2 result page (resultw2.html) which implements the Warmly version 2 google doc.  Currenlty it loads the synonym network diagram from the checked in 'js/testdata.js' which replicates the warmly v2 web response.  The other UI elments have hardcoded placeholder sample data.

---
+++ js/testdata.js
@@ -0,0 +1,59 @@
+var testdata = {
+  "connectors": [
+    "Success", 
+    "Entrepreneur",
+    "Education"
+  ], 
+  "identifiers": [
+    "UMBC", 
+    "education technology"
+  ], 
+  "mips": [
+    {
+      "m": "To successfully manage a startup team you must use technology that enables your employees to achieve success.", 
+      "r": "http://demo.demo.test.com/somearticle.html"
+    }
+  ], 
+  "radar": {
+    "datasets": [
+      {
+        "data": [
+          51, 
+          25, 
+          39
+        ]
+      }
+    ], 
+    "label": [
+      "s1", 
+      "s2", 
+      "s3"
+    ]
+  }, 
+  "snippet": "test message", 
+  "synonym": [
+    {
+      "root": "success", 
+      "syn": [
+        "achievement", 
+        "accomplishment", 
+        "winning"
+      ]
+    }, 
+    {
+      "root": "graduation", 
+      "syn": [
+        "commencement", 
+        "convocation"
+      ]
+    },
+    {
+      "root": "retention", 
+      "syn": [
+        "persistence", 
+        "attainment"
+      ]
+    }
+  ], 
+  "target": "John Fritz"
+};
