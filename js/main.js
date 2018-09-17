7938188609901a57f98ba82399cd124c743a9115
Chris Wolf <cw10025@gmail.com>
2018-09-16 21:37:15 -0400

    Implement and test new polling protocol.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var gmail;
 
 //var service_base_url='http://warmly-env-2.bq6ng3tn7d.us-east-2.elasticbeanstalk.com';
 var service_base_url='https://www.warmly.io'
-var api_path='/api/1.0/test';
+var api_path='/api/1.0';
 var warmly_service_url=service_base_url + api_path;
 
 // this is now loaded from 'criteria_dialog.html'
@@ -19,6 +19,10 @@ var criteria_dialog = "";
 
 // TODO: externalize
 var warmly_btn_label = "Warm";
+
+// TODO: this use of a global if pretty bad, but not sure of new protocol...
+var g_request_id = null;
+var g_poll_count = 0;
 
 function refresh(warmly_entry_point) {
   if( (/in/.test(document.readyState)) || (typeof Gmail === undefined) ) {
@@ -29,37 +33,50 @@ function refresh(warmly_entry_point) {
   }
 }
 
-function call_warmly(search_criteria) {
+function call_warmly(resource_path, search_criteria) {
+  var polling = false;
   $.support.cors = true;
   $.ajax({
-      'url' : warmly_service_url,
+      'url' : warmly_service_url + resource_path,
       'type': 'GET',
       'dataType' : 'json',
       'data': search_criteria,
       'success' : function(data, textStatus, xhr) {
-          console.log('====>> HTTP status: ' + xhr.status); 
-          if (xhr.status == 204) {
+          console.log('====>> poll: ' + g_poll_count + ' HTTP status: '
+              + xhr.status + ' requestId: ' + g_request_id);
+          if (xhr.status == 204 || data == null) {
              console.log('====>> No data');
-          } else if ('results' in data) {
-              console.log('====>> results: ' + data['results']);
+             if (g_request_id != null) {
+                 polling = true;  // not sure this is correct
+             } else {
+                 polling = false;
+             }
+             g_poll_count++;
           } else if ('connectors' in data) {
               console.log('====>> connectors: ' + data['connectors']);
+              polling = false; // we're done, we have the data
+              g_request_id = null;
+              g_poll_count = 0;
           } else if ('requestId' in data) {
               console.log('====>> requestId: ' + data['requestId']);
-              setTimeout(function() {
-                  call_warmly({'requestId': data['requestId']});
-              }, 3000);
+              g_request_id =  data['requestId'];
+              polling = true;
+              g_poll_count = 0;
           }
 
-          if (data == null) {
-              data = new Object();
+          if (polling) {
+              setTimeout(function() {
+                  call_warmly('/articles/polling', {'requestId': g_request_id});
+              }, 3000);
+              return;
           }
-          data.search_criteria = search_criteria;
+
+          //data.search_criteria = search_criteria;
           var payload = {type: 'warmly_display_result', data: data};
           console.log('====>> payload type: ' + payload.type + ' payload data: ' + payload.data);
-          setTimeout(function() {
-              chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', payload);
-          }, 1000);
+          //setTimeout(function() {
+          chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', payload);
+          //}, 1000);
       },
       'error' : function(jqXHR, status, message) {
           console.log('====> service call error: ' + status + ': ' + message);
@@ -187,7 +204,7 @@ var main = function() {
           + ' tags: ' + args.tags + ' closing: ' + args.closing);
       chrome.runtime.sendMessage('papmjbnpmffiahcnakjfjoobkefaemii', {type:'warmly_create_popup'});
       console.log('====> sent message to request popup');
-      call_warmly(args);
+      call_warmly('/articles', args);
       console.log('====> called warmly.');
       gmail.tools.remove_modal_window();
   };
